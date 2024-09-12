484b982ad2d132080fc187e786672d2855a20da0
Sofiegreen <sofgre596@ihmeducation.se>
2024-09-12 10:52:35 +0200

    CreatedP4Variables

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -2,3 +2,15 @@ console.log("Hello World!");
 console.log("My name is Sofie");
 console.log("testing testing");
 console.log("Funkar detta?");
+// This is a comment.
+
+const five= 5;
+const eight=8;
+let total=0;
+console.log(five);
+console.log(eight);
+console.log(five + eight);
+
+total = five + eight;
+console.log(total);
+// 5 + 8 = 13
