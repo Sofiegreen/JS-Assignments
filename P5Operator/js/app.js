c3a33696e0b17d05c700cf7b342b1042491a3292
Sofiegreen <sofgre596@ihmeducation.se>
2024-09-12 15:30:00 +0200

    Math Assignment Part 1

---
+++ P5Operator/js/app.js
@@ -0,0 +1,31 @@
+let speedInput= 72
+let speedOutput = 0
+
+speedOutput = speedInput /3.6;
+console.log(speedOutput);
+
+let minutesInput = 3;
+let  secondsOutput = 0;
+secondsOutput = 60 * minutesInput;
+console.log(secondsOutput);
+
+let minutesInput1 = 11
+let minutesImput2= 4
+let divisionResult = 0
+
+divisionResult = minutesInput1 / minutesImput2;
+console.log(divisionResult);
+
+let resultinput1 = 11;
+let resultinput2 = 4;
+let result=0;
+
+result = resultinput1 % resultinput2;
+console.log(result);
+
+let circleNumber = 2
+let circleResult= 0
+
+
+
+
