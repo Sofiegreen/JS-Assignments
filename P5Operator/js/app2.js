d54f63de3804f2ff782d84d60dcfd95ad4d90952
Sofiegreen <sofgre596@ihmeducation.se>
2024-09-12 14:41:45 +0200

    create app2

---
+++ P5Operator/js/app2.js
@@ -0,0 +1,66 @@
+let x = 5 + 5; // x = 10
+let y = x + 5; // y = 15
+let z = y + y; // z = 30
+console.log(z + z); // 60
+
+let firstnumber= 7;
+let secondnumber= 2;
+let seven= 7;
+let answer=0;
+
+console.log(firstnumber+secondnumber);
+answer= firstnumber + secondnumber;
+console.log(answer);
+
+answer=firstnumber-secondnumber;
+console.log(answer);
+
+answer = firstnumber * secondnumber;
+console.log(answer);
+answer = firstnumber / secondnumber;
+console.log(answer);
+
+console.log(seven);
+
+answer = firstnumber % secondnumber;
+console.log(answer);
+
+let gold= 50;
+gold + 10;
+console.log(gold);
+
+gold = gold + 10;
+console.log(gold);
+
+gold += 10;
+console.log(gold);
+
+gold /= 10;
+console.log(gold);
+
+gold %= 2;
+console.log(gold);
+
+gold -= 10;
+console.log(gold);
+
+gold *= 10;
+console.log(gold);
+
+let numberOfStudents = 1;
+// numberOfStudents += 1;
+// console.log(numberOfStudents);
+
+numberOfStudents++;
+console.log(numberOfStudents);
+
+++numberOfStudents;
+console.log(numberOfStudents);
+
+console.log(numberOfStudents++); // 3 WHY?
+console.log(numberOfStudents); // 4
+
+console.log(++numberOfStudents); // 5
+
+// numberOfStudents--;
+// console.log(numberOfStudents);
