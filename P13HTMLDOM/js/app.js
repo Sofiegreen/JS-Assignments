d85b711be11d09b0c7c868b1d2b664966276e9ac
Sofiegreen <sofgre596@ihmeducation.se>
2024-09-17 17:22:03 +0200

    Creating a website

--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -6,6 +6,9 @@ textField0.innerHTML= "Sofie";
 const headerText5=document.getElementById('header5');
 headerText5.innerHTML= "THIS IS MY WEBSITE!!!"
 
+const randomNumber0= document.getElementById("randomNumber0");
+randomNumber0.innerHTML = chooseANumber(3).toString();
+
 headerText5.style.color= "pink";
 textField0.style.color= "blue";
 
@@ -15,3 +18,13 @@ button0.addEventListener('click', () => {yourFunctionHere();
 
 button0.style.backgroundColor= "red";
 button0.style.fontSize="100px";
+textField0.innerHTML= multiplyNySelf(9)
+
+function multiplyNySelf(number)  {
+  return number * number;
+}
+
+function chooseANumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
