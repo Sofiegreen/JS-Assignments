bb55894e1f01240672afdec9887d075a74f04f8c
Sofiegreen <sofgre596@ihmeducation.se>
2024-09-17 14:26:28 +0200

    P13HTMLDOM

---
+++ P13HTMLDOM/js/app.js
@@ -0,0 +1,17 @@
+// Reference "text0" and assign it to the variable called "textField0"
+
+const textField0= document.getElementById('text0');
+textField0.innerHTML= "Sofie";
+
+const headerText5=document.getElementById('header5');
+headerText5.innerHTML= "THIS IS MY WEBSITE!!!"
+
+headerText5.style.color= "pink";
+textField0.style.color= "blue";
+
+const button0= document.getElementById('button0');
+button0.addEventListener('click', () => {yourFunctionHere();
+})
+
+button0.style.backgroundColor= "red";
+button0.style.fontSize="100px";
