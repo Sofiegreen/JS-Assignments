295c99a4a3da9ba138ec39cbec04821154c310d7
Sofiegreen <sofgre596@ihmeducation.se>
2024-09-19 11:08:08 +0200

    Created a button, when pressing the number gets higher

--- AnythingYouWant/js/app.js
+++ AnythingYouWant/js/app.js
@@ -1,13 +1,23 @@
+// Document elements
 // Looks for "text0". Assigns to "textField0".
 const textField0= document.getElementById("text0");
-textField0.innerHTML= showStylizedScore( 786)
 
-function showScore(inputNumber) {
-  return inputNumber * 100; // Output is a number.
+const increaseScoreButton= document.getElementById("button0");
+
+// internal variables
+let score= 0;
+
+// Process (what is going to happen in what sequence?)
+increaseScoreButton.addEventListener("click", () => {
+  increaseScoreByOne();
+});
+
+function increaseScoreByOne() {
+  score++;
+  updateScoreText(score);
 }
 
-function showStylizedScore(scoreInput) {
-  return "Your Score is: " + scoreInput * 100; // output is a string + number
+//View
+function updateScoreText(newText) {
+  textField0.innerHTML= newText;
 }
-
-
