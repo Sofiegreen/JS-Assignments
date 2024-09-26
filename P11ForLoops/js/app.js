da1e9af160aec63b288d96d061f3e10b6273b179
Sofiegreen <sofgre596@ihmeducation.se>
2024-09-26 09:02:03 +0200

    P11 For Loops

---
+++ P11ForLoops/js/app.js
@@ -0,0 +1,44 @@
+// // function loopRandomNumber(max) {
+// //   return Math.floor(Math.random() * max);
+// // }
+// //
+// // for (let i = 0; i < 13; i++) {
+// //   console.log(loopRandomNumber(6));
+// // }
+//
+// // for (let i = 1; i <= 1024; i *= 2) {
+// //   console.log(i)
+// // }
+//
+// // let startMultiplying= 1;
+// // let stopMultiplying= 1024;
+// //
+// // for (let i = 1; i <= 1024; i++) {
+// //   console.log(i * 2)
+// // }
+// //
+// // for (let i = 10; i >= 0; i--) {
+// //  console.log(i);
+// // }
+//
+//
+// function power(firstNumber, exponent) {
+//   let result = firstNumber;
+//   for (let i = 1; i < exponent; i++) {
+//     result *= firstNumber;
+//   }
+//   return result;
+// }
+// console.log(power(4, 24));
+
+//
+// for (let i = 999; i >= 0 ; i--) {
+//   console.log(i + " green bottles hanging on the wall" + i + " green bottles hanging on the wall" + " and if one green bottle, should accidentally fall, there will be " +
+//     i + " green bottles hanging on the wall.");
+// }
+
+let item1;
+let item2;
+let item3;
+let item4;
+
